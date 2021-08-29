faec97ac3ccfa3495285debe877919631c2332b5
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-08-29 08:54:13 +0530

    updated cart logic as per naveen review comments and created cart util

--- src/main/java/com/qa/opencart/pages/CartPage.java
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -6,6 +6,10 @@ public class CartPage {
 	
 	By cart = By.id("cart");
 	
+	public void addToCart() {
+		System.out.println("add the product to the cart");
+	}
+	
 	
 
 }
--- src/test/java/com/qa/opencart/tests/CartTest.java
+++ src/test/java/com/qa/opencart/tests/CartTest.java
@@ -4,11 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CartTest {
-	
+
 	@Test
 	public void testCart() {
+		System.out.println("add to cart step");
 		Assert.assertEquals(true, true);
 	}
-	
 
 }
