248b01239033d71378bcfa9dbc6fa273b96b45f9
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-08-29 08:36:37 +0530

    cart feature added

---
+++ src/main/java/com/qa/opencart/pages/CartPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class CartPage {
+	
+	By cart = By.id("cart");
+	
+	
+
+}
---
+++ src/test/java/com/qa/opencart/tests/CartTest.java
@@ -0,0 +1,14 @@
+package com.qa.opencart.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest {
+	
+	@Test
+	public void testCart() {
+		Assert.assertEquals(true, true);
+	}
+	
+
+}
