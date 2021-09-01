ed4dc330c0a435b1f0a7d5a70569cca2ad8cdb21
Naveen AutomationLabs <naveenanimation20@gmail.com>
2021-09-01 07:25:14 +0530

    updated login page test

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -45,7 +45,7 @@ public class LoginPageTest extends BaseTest {
 		Assert.assertTrue(loginPage.isForgotPwdLinkExist());
 	}
 
-	@Description("login page test.....")
+	@Description("login page test with username and password.....")
 	@Severity(SeverityLevel.BLOCKER)
 	@Test(priority = 4)
 	public void loginTest() {
